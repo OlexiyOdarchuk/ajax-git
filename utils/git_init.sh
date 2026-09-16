199d9afe70439dc7e2faefa1d03e1a8dedb11aba
iShawyha <shawyhaf@gmail.com>
2026-09-16 21:17:46 +0300

    feat: add load_config() func and info/error

--- utils/git_init.sh
+++ utils/git_init.sh
@@ -1,9 +1,21 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -u
 
+CONFIG_FILE=".git_myconfig"
 SCRIPT_NAME="$(basename "$0")"
 
+info()
+{
+    echo "[INFO] $1"
+}
+
+error()
+{
+    echo "[ERROR] $1" >&2
+}
+
+usage()
 {
     cat <<EOF
 Usage:
@@ -29,3 +41,20 @@ Description:
         the specified remote repository.
 EOF
 }
+
+load_config()
+{
+    source "$CONFIG_FILE"
+
+    if [[ -z "${USER_NAME:-}" ||
+          -z "${USER_EMAIL:-}" ||
+          -z "${USER_BRANCH:-}" ]]; then
+        error "Configuration file is incomplete."
+        error "Required variables: USER_NAME, USER_EMAIL, USER_BRANCH."
+        return 1
+    fi
+    info "All is good"
+    return 0
+}
+
+load_config
